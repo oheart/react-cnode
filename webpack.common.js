f0ff59267184ba5116355b264a9bb3fc809a6d65
oheart <2353769123@qq.com>
2018-03-06 12:01:40 +0800

    modify config

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const entryPath = path.resolve(__dirname, 'src');
 const devPath = path.resolve(__dirname, 'dist');
-const publicPath = '';
+const publicPath = './';
 
 
 module.exports = {
